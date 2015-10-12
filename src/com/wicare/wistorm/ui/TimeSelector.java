f7455bd0df27e7b0132819009322f6d055a07478
Administrator <Administrator@PC--20150902QLW>
2015-10-12 11:13:20 +0800

    TimeSelector

--- src/com/wicare/wistorm/ui/TimeSelector.java
+++ src/com/wicare/wistorm/ui/TimeSelector.java
@@ -18,4 +18,52 @@ import android.widget.TimePicker;
  */
 public class TimeSelector {
 
+	private Context mContext;
+	private int mHour,mMinute;
+	
+	public TimeSelector(Context context){
+		
+		mContext = context;
+		// 获取系统本机当前的时间 
+        final Calendar c = Calendar.getInstance();  
+        mHour = c.get(Calendar.HOUR_OF_DAY);  
+        mMinute = c.get(Calendar.MINUTE);  
+	}
+	
+	public void setTime(final TextView tv,final EditText ed){
+		TimePickerDialog timePickerDialog = new TimePickerDialog(mContext, 
+				new OnTimeSetListener() {						   
+		    @Override
+			public void onTimeSet(TimePicker view,
+					int hourOfDay, int minute) {
+				// TODO Auto-generated method stub
+		    	setmHour(hourOfDay);
+		    	setmMinute(minute);
+		    	if(tv!=null){
+		    		tv.setText(getmHour() + ":" + getmMinute());		
+		    	}
+		    	if(ed!=null){
+		    		ed.setText(getmHour() + ":" + getmMinute());
+		    	}
+			}
+		},mHour, mMinute, true);
+		timePickerDialog.setTitle(R.string.set_time);
+		timePickerDialog.show();
+	}
+
+	private int getmHour() {
+		return mHour;
+	}
+
+	private void setmHour(int mHour) {
+		this.mHour = mHour;
+	}
+
+	private int getmMinute() {
+		return mMinute;
+	}
+
+	private void setmMinute(int mMinute) {
+		this.mMinute = mMinute;
+	}
 }
