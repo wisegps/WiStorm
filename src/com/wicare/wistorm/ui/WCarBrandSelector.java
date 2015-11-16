ba14ea71bf46a070ccfb618fdfe971799ca2f4cc
Administrator <Administrator@PC--20150902QLW>
2015-11-16 17:50:19 +0800

    汽车选择

--- src/com/wicare/wistorm/ui/WCarBrandSelector.java
+++ src/com/wicare/wistorm/ui/WCarBrandSelector.java
@@ -1,5 +1,12 @@
 package com.wicare.wistorm.ui;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -12,21 +19,28 @@ import org.json.JSONObject;
 import com.wicare.wistorm.R;
 import com.wicare.wistorm.http.HttpThread;
 import com.wicare.wistorm.model.CarBrandData;
-import com.wicare.wistorm.ui.WCarSelectorSideBar.OnTouchingLetterChangedListener; 
+import com.wicare.wistorm.ui.WCarSelectorSideBar.OnTouchingLetterChangedListener;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -40,37 +54,48 @@ import android.widget.Toast;
 public class WCarBrandSelector extends Activity implements OnClickListener, TextWatcher {
 	
 	static final String TAG = "WBrandSelector";
+	/** 图片路径存储地址 **/
+	public static String BasePath = Environment.getExternalStorageDirectory().getPath() + "/wistorm/";
+	/** 车品牌logo **/
+	public static String VehicleLogoPath = BasePath + "vehicleLogo/";
+	/** 图片地址 **/
+	public static String ImageUrl = "http://img.wisegps.cn/logo/";
+	static final int CAR_SELECTOR_RESULT_CODE = 1;
 	static final String carBrand_url  = "http://api.bibibaba.cn/base/car_brand";
-	
 	static final String carSeries_url = "http://api.bibibaba.cn/base/car_series?pid=";
-	
+	static final String cartypes_url  = "http://api.bibibaba.cn/base/car_type?pid=";
 	private static final int GET_CAR_BRAND_DATA  = 1;
 	private static final int GET_CAR_Series_DATA = 2;
-	
+	private static final int GET_CAR_Types_DATA  = 3;
+	private static final int GET_Logo_IMAGE      = 4;
 	/** 品牌 **/
 	private List<CarBrandData> brandDatas = new ArrayList<CarBrandData>(); // 车辆品牌集合
 	/** 车型 **/
 	private List<String[]> carSeries = new ArrayList<String[]>();
 	/** 车款 **/
 	private List<String[]> carTypes = new ArrayList<String[]>();
-	
-	private TextView letterIndex = null; // 字母索引选中提示框
+	private TextView tv_title = null;
+	private TextView letterIndex = null;// 字母索引选中提示框
 	private WCarSelectorSideBar sideBar;//侧旁的字母列表
 	private WPingYinUtil characterParser; // 将汉字转成拼音
 	private ListView carBrandListView;//汽车品牌列表
 	private WInputField etSearchCarKey;//搜索框
 	private WCarBrandAdapter mCarBrandAdapter;//汽车品牌列表适配器
-	
+	private WLoading mWLoading = null;
 	private ListView carModelListView; //汽车型号列表
+	private ListView carTypelListView; //汽车款式列表
+	boolean currentSeriesState = false;//当前页面是选择车型？
+	boolean currentTypesState  = false;//当前页面是选择车款？
+	private boolean imageDownload = true;
+	private Imagehread imageThread = null;
 	
-	boolean isNeedType = true;
-	boolean isNeedModel = true;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ws_brand_selector);
 		initView();
+		startProgressDialog();//加载弹框
 	}
 	
 	
@@ -82,18 +107,21 @@ public class WCarBrandSelector extends Activity implements OnClickListener, Text
 		ivBack.setOnClickListener(this);
 		
 		carModelListView = (ListView) findViewById(R.id.lv_car_model);
+		carModelListView.setOnItemClickListener(onSeriesClickListener);
 		
+		carTypelListView = (ListView) findViewById(R.id.lv_car_type);
+		carTypelListView.setOnItemClickListener(onTypesClickListener);
 		
 		etSearchCarKey = (WInputField) findViewById(R.id.et_car_key);
 		etSearchCarKey.addTextChangedListener(this);
 		carBrandListView = (ListView) findViewById(R.id.lv_car_brand);
 		carBrandListView.setOnItemClickListener(onBrankClickListener);
 		characterParser = new WPingYinUtil();
+		tv_title = (TextView) findViewById(R.id.tv_car_select_title);
 		letterIndex = (TextView) findViewById(R.id.tv_car_alpha_overlay_bg);
 		sideBar = (WCarSelectorSideBar) findViewById(R.id.sb_letter);
 		sideBar.setTextView(letterIndex);
 		sideBar.setOnTouchingLetterChangedListener(new OnTouchingLetterChangedListener() {
-			
 			@Override
 			public void onTouchingLetterChanged(String s) {
 				// TODO Auto-generated method stub
@@ -126,43 +154,75 @@ public class WCarBrandSelector extends Activity implements OnClickListener, Text
 			carBrankId = brandDatas.get(position).getId();
 			logoUrl    = brandDatas.get(position).getLogoUrl();
 			Log.e(TAG, carBrank + "--" + carBrankId + "--" + logoUrl);
-			
-			if (isNeedModel) {
-				// 点击品牌列表 选择车型
-				getSeriesData(carBrankId);
-			} else {
-			
-				//返回
-				
-			}
-			
+			getSeriesData(carBrankId);
+			startProgressDialog();//弹框
+		}
+	};
+	
+	String carSeriesId;
+	String carSerie;
+	/** 汽车型号点击事件 **/
+	OnItemClickListener onSeriesClickListener = new OnItemClickListener() {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {	
+			carSeriesId = carSeries.get(position)[0];
+			carSerie    = carSeries.get(position)[1];
+			Log.e(TAG, carSerie + "--" + carSeriesId);
+			getTypeData(carSeriesId);
+			startProgressDialog();//弹框
 		}
 	};
 	
 	
-	
-	
+	/** 汽车款式点击事件 **/
+	OnItemClickListener onTypesClickListener = new OnItemClickListener() {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {	
+			//把数据返回
+			Intent intent = new Intent();
+			intent.putExtra("brank", carBrank);
+			intent.putExtra("brankId", carBrankId);
+			intent.putExtra("series", carSerie);
+			intent.putExtra("seriesId", carSeriesId);
+			intent.putExtra("typeId", carTypes.get(position)[0]);
+			intent.putExtra("type", carTypes.get(position)[1]);
+			intent.putExtra("logoUrl", logoUrl);
+            WCarBrandSelector.this.setResult(CAR_SELECTOR_RESULT_CODE,intent);
+            WCarBrandSelector.this.finish();
+		}
+	};
 	
 
+	/**
+	 *  返回按钮设置
+	 */
 	@Override
 	public void onClick(View v) {
-		
-		if(v.getId() == R.id.iv_back){
+		if(currentSeriesState){
+			currentSeriesState = false;
+			carBrandListView.setVisibility(View.VISIBLE);
+			sideBar.setVisibility(View.VISIBLE);
+			etSearchCarKey.setVisibility(View.VISIBLE);
+			carModelListView.setVisibility(View.GONE);
+			tv_title.setText(getResources().getString(R.string.car_brand_title));
+		}else if(currentTypesState){
+			currentSeriesState = true;
+			currentTypesState  = false;
+			carTypelListView.setVisibility(View.GONE);
+			carModelListView.setVisibility(View.VISIBLE);
+			tv_title.setText(getResources().getString(R.string.car_series_title));
+		}else{
 			WCarBrandSelector.this.finish();
 		}
-		
 	}
 
-	
-	
-	
-	
-	
-	
 	
 	/**
      * Handler 处理消息
      */
+	@SuppressLint("HandlerLeak") 
 	private Handler mHandler = new Handler() {
     	
         @Override
@@ -180,7 +240,27 @@ public class WCarBrandSelector extends Activity implements OnClickListener, Text
                 break;
                 
             case GET_CAR_Series_DATA:
+            	if(msg.obj.toString().indexOf("Exception") != -1){//有异常
+                    Toast.makeText(WCarBrandSelector.this, "请求网络失败", Toast.LENGTH_SHORT).show(); 
+                    return;
+                }else{
+                	jsonSeries(msg.obj.toString());
+                }
             	
+            	break;
+            	
+            case GET_CAR_Types_DATA:
+            	if(msg.obj.toString().indexOf("Exception") != -1){//有异常
+                    Toast.makeText(WCarBrandSelector.this, "请求网络失败", Toast.LENGTH_SHORT).show(); 
+                    return;
+                }else{
+                	jsonTypes(msg.obj.toString());
+                }
+            	break;
+            	
+            case GET_Logo_IMAGE:
+            	mCarBrandAdapter.notifyDataSetChanged();
+            	Log.e(TAG,"mCarBrandAdapter.notifyDataSetChanged();-------");
             	break;
             }
         }
@@ -217,7 +297,10 @@ public class WCarBrandSelector extends Activity implements OnClickListener, Text
 			mCarBrandAdapter = new WCarBrandAdapter(this);
 			mCarBrandAdapter.setData(brankList);
 			carBrandListView.setAdapter(mCarBrandAdapter);
-			Log.e(TAG, brankList.size() + "");
+			// 刷新品牌logo
+			imageThread = new Imagehread();
+			imageThread.start();
+			stopProgressDialog();//关闭弹框
 		} catch (JSONException e2) {
 			e2.printStackTrace();
 		}
@@ -227,11 +310,11 @@ public class WCarBrandSelector extends Activity implements OnClickListener, Text
     /**
      * @param result 车型json格式数据
      */
-    private void jsonSeries(String result) {
+    private void jsonSeries(String jsonSeriesData) {
 		carSeries.clear();
 		JSONArray jsonArray;
 		try {
-			jsonArray = new JSONArray(result);
+			jsonArray = new JSONArray(jsonSeriesData);
 			int jsonLength = jsonArray.length();
 			for (int i = 0; i < jsonLength; i++) {
 				String[] series = new String[2];
@@ -244,10 +327,15 @@ public class WCarBrandSelector extends Activity implements OnClickListener, Text
 				}
 			}
 			// 隐藏车牌列表 显示车型列表
+			currentSeriesState = true;
 			carBrandListView.setVisibility(View.GONE);
-//			ArrayAdapter<String> adapterModel = new ArrayAdapter<String>(context, resource);
-			
+			sideBar.setVisibility(View.GONE);
+			etSearchCarKey.setVisibility(View.GONE);
+			tv_title.setText(getResources().getString(R.string.car_series_title));
+			carSeriesAndTypeAdapter adapter = new carSeriesAndTypeAdapter(this, carSeries);
+			carModelListView.setAdapter(adapter);
 			carModelListView.setVisibility(View.VISIBLE);
+			stopProgressDialog();//关闭弹框
 		} catch (JSONException e1) {
 			e1.printStackTrace();
 		}
@@ -255,6 +343,53 @@ public class WCarBrandSelector extends Activity implements OnClickListener, Text
 	}
     
     /**
+     * @param carSeriesId 车型ID
+     */
+    private void jsonTypes(String carSeriesResult){
+    	JSONArray jsonArray = null;
+		try {
+			jsonArray = new JSONArray(carSeriesResult);
+			int jsonTypeLength = jsonArray.length();
+			for (int i = 0; i < jsonTypeLength; i++) {
+				String[] typeStr = new String[2];
+				try {
+					typeStr[0] = jsonArray.getJSONObject(i).getString("id");
+					typeStr[1] = jsonArray.getJSONObject(i) .getString("go_name")
+							+ "  "
+							+ jsonArray.getJSONObject(i).getString("name");
+					carTypes.add(typeStr);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+			currentSeriesState = false;
+			currentTypesState = true;
+			carModelListView.setVisibility(View.GONE);
+			tv_title.setText(getResources().getString(R.string.car_type_title));
+			carSeriesAndTypeAdapter adapter = new carSeriesAndTypeAdapter(this, carTypes);
+			carTypelListView.setAdapter(adapter);
+			carTypelListView.setVisibility(View.VISIBLE);
+			stopProgressDialog();//关闭弹框
+			if (jsonTypeLength == 0) {
+				Intent intent = new Intent();
+				intent.putExtra("brank", carBrank);
+				intent.putExtra("brankId", carBrankId);
+				intent.putExtra("series", carSerie);
+				intent.putExtra("seriesId", carSeriesId);
+				intent.putExtra("typeId", "");
+				intent.putExtra("type", "");
+				intent.putExtra("logo", logoUrl);
+				WCarBrandSelector.this.setResult(CAR_SELECTOR_RESULT_CODE, intent);
+				WCarBrandSelector.this.finish();
+			}
+			
+		} catch (JSONException e2) {
+			e2.printStackTrace();
+		}
+    }
+    
+    /**
+     * 获取车品牌
      * @param carBrandId 	汽车id
      */
     private void getSeriesData(String carBrandId){
@@ -264,7 +399,16 @@ public class WCarBrandSelector extends Activity implements OnClickListener, Text
 		new HttpThread.getDataThread(mHandler, url, GET_CAR_Series_DATA).start();
     }
     
-    
+    /**
+	 * 获取车款
+	 * @param carBrankId
+	 */
+	private void getTypeData(String carSeriesId) {
+		String url = cartypes_url + carSeriesId;
+		Log.e("获取车款", url);
+		//开启线程获取服务器数据
+		new HttpThread.getDataThread(mHandler, url, GET_CAR_Types_DATA).start();
+	}
     
 	/**
 	 * a-z排序
@@ -291,7 +435,7 @@ public class WCarBrandSelector extends Activity implements OnClickListener, Text
 	 * @param date
 	 * @return
 	 */
-	private List<CarBrandData> filledData(List<CarBrandData> brankList) {
+	@SuppressLint("DefaultLocale") private List<CarBrandData> filledData(List<CarBrandData> brankList) {
 		for (int i = 0; i < brankList.size(); i++) {
 			// 汉字转换成拼音
 			@SuppressWarnings("static-access")
@@ -342,7 +486,119 @@ public class WCarBrandSelector extends Activity implements OnClickListener, Text
 	}
 	
 	
+	/**
+	 * 开始显示加载
+	 */
+	private void startProgressDialog() {
+		if (mWLoading == null) {
+			mWLoading = WLoading.createDialog(this);
+			mWLoading.setMessage("数据加载中...");
+		}
+		mWLoading.show();
+	}
+
+	/**
+	 * 关闭加载提示
+	 */
+	private void stopProgressDialog() {
+		if (mWLoading != null) {
+			mWLoading.dismiss();
+			mWLoading = null;
+		}
+	}
 	
+	
+	
+	
+	public void logoImageIsExist(final String imagePath, final String name,
+			final String logoUrl) {
+		// 去掉最后面的斜杠
+		File filePath = new File(imagePath);
+		if (!filePath.exists()) {
+			filePath.mkdirs();
+		}
+		File imageFile = new File(imagePath + name + ".png");
+		if (!imageFile.exists()) {
+			Bitmap bitmap = getBitmapFromURL(ImageUrl + logoUrl);
+			if (bitmap != null) {
+				createImage(imagePath + name + ".png", bitmap);
+			}
+		}
+	}
+
+	/**
+	 * @author Wu
+	 * 开线程获取图片
+	 */
+	class Imagehread extends Thread {
+		public void run() {
+			for (int i = 0; i < brandDatas.size(); i++) {
+				if (!"".equals(brandDatas.get(i).getLogoUrl())
+						&& brandDatas.get(i).getLogoUrl() != null) {
+					if (imageDownload) {
+						logoImageIsExist(VehicleLogoPath, brandDatas.get(i).getId(), brandDatas.get(i).getLogoUrl());
+					} else {
+						continue;
+					}
+				}
+			}
+			super.run();
+		}
+
+		public void reStart() {
+			run();
+		}
+	}
+
+	// 向SD卡中添加图片
+	public void createImage(String fileName, Bitmap bitmap) {
+		FileOutputStream b = null;
+		try {
+			b = new FileOutputStream(fileName);
+			bitmap.compress(Bitmap.CompressFormat.PNG, 100, b);// 把数据写入文件
+			Message msg = new Message();
+			msg.what = GET_Logo_IMAGE;
+			mHandler.sendMessage(msg);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				b.flush();
+				b.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * 从服务器读取图片
+	 * 
+	 * @param Path
+	 * @return Bitmap
+	 */
+	public Bitmap getBitmapFromURL(String Path) {
+		Log.e(TAG,"getBitmapFromURL(String Path);-------");
+		try {
+			URL url = new URL(Path);
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+			connection.setDoInput(true);
+			connection.connect();
+			InputStream input = connection.getInputStream();
+			Bitmap myBitmap = BitmapFactory.decodeStream(input);
+			return myBitmap;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+	protected void onPause() {
+		imageDownload = false;
+		super.onPause();
+	}
     
 	/**
 	 * 搜索框输入监听
@@ -361,6 +617,60 @@ public class WCarBrandSelector extends Activity implements OnClickListener, Text
 	
 	@Override
 	public void afterTextChanged(Editable s) {
+	}
+	
+	
+	/**
+	 * @author Wu
+	 * 车型和车款listview 适配器
+	 */
+	public class carSeriesAndTypeAdapter extends BaseAdapter{
+		
+		private Context mContext;
+		private List<String[]> carSeriesList;
+		
+		public carSeriesAndTypeAdapter(Context context,List<String[]> seriesList){
+			this.mContext = context;
+			this.carSeriesList = seriesList;
+		}
+
+		@Override
+		public int getCount() {
+			return carSeriesList.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return carSeriesList.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@SuppressLint("InflateParams") @Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = null;
+			viewHolder holder;
+			if(convertView == null){
+				view = LayoutInflater.from(mContext).inflate(R.layout.ws_car_series_and_type_item, null);
+				holder = new viewHolder();
+				holder.tv_car = (TextView) view.findViewById(R.id.tv_car_series_type);
+				view.setTag(holder);
+			}else {
+				view = convertView;
+				holder = (viewHolder)view.getTag();
+			}
+			
+			holder.tv_car.setText(carSeriesList.get(position)[1]);
+			return view;
+		}
+
+		class viewHolder{
+			TextView tv_car;
+		}
+		
 	}
      
 }
