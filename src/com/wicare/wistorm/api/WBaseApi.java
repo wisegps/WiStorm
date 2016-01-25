a9d416261a79f506086ddfc8034e0b02cbbbd495
c <c@c-PC>
2016-01-25 16:43:07 +0800

    车辆api

--- src/com/wicare/wistorm/api/WBaseApi.java
+++ src/com/wicare/wistorm/api/WBaseApi.java
@@ -1,4 +1,20 @@
 package com.wicare.wistorm.api;
+
+import java.util.HashMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Handler.Callback;
+import android.util.Log;
+
+import com.wicare.wistorm.http.BaseVolley;
+import com.wicare.wistorm.http.Msg;
+
 /**
  * BaseApi
  * 
@@ -7,4 +23,125 @@ package com.wicare.wistorm.api;
  */
 public class WBaseApi extends WiStormAPI {
 
-}
+	public String Method_Base_Brands = "wicare.base.car_brands.list";//获取品牌列表(非活跃数据1d)
+	public String Method_Base_Series = "wicare.base.car_series.list";//获取车系列表(非活跃数据1d)
+	public String Method_Base_types = "wicare.base.car_types.list";//获取车款列表
+	public HashMap<String, String> hashParam = new HashMap<String, String>();
+
+	public Context context;
+	private Handler uiHandler;// UI线程
+	private Handler workHandler;// 工作线程，非UI线程
+	private BaseVolley volley;
+
+	public WBaseApi(Handler uiHandler) {
+		this.uiHandler = uiHandler;
+		init();
+	}
+
+	/**
+	 * 初始化数据
+	 * 
+	 * @param uiHandler
+	 */
+	public void init() {
+		this.workHandler = super.initWorkHandler(handleCallBack);
+		volley = new BaseVolley(workHandler);
+	}
+
+	/**
+	 * 工作子线程回调函数,负责接收网络返回数据，并交给解析函数进一步处理
+	 */
+	private Callback handleCallBack = new Callback() {
+		@Override
+		public boolean handleMessage(Message msg) {
+			switch (msg.what) {
+			case Msg.M_Base_Brands:
+				parseBrands(msg);
+				break;
+			case Msg.M_Base_Series:
+				parseSeries(msg);
+				break;
+			case Msg.M_Base_Types:
+				parseTypes(msg);
+				break;
+			}
+			return false;
+		}
+	};
+
+	/***************************************************** 解析网络返回的数据 ********************************/
+	/**
+	 * 解析返回的数据
+	 * 
+	 * @param msg
+	 */
+	private void parseBrands(Message msg) {
+			Message uimsg = uiHandler.obtainMessage();
+			uimsg.what = msg.what;
+			Bundle bundle = msg.getData();
+			bundle.putString("brands", msg.obj.toString());
+			uimsg.setData(bundle);
+			uiHandler.sendMessage(uimsg);
+	}
+	
+	/**
+	 * 解析返回的数据
+	 * 
+	 * @param msg
+	 */
+	private void parseSeries(Message msg) {
+			Message uimsg = uiHandler.obtainMessage();
+			uimsg.what = msg.what;
+			Bundle bundle = msg.getData();
+			bundle.putString("series", msg.obj.toString());
+			uimsg.setData(bundle);
+			uiHandler.sendMessage(uimsg);
+	}
+	
+	/**
+	 * 解析返回的数据
+	 * 
+	 * @param msg
+	 */
+	private void parseTypes(Message msg) {
+			Message uimsg = uiHandler.obtainMessage();
+			uimsg.what = msg.what;
+			Bundle bundle = msg.getData();
+			bundle.putString("types", msg.obj.toString());
+			uimsg.setData(bundle);
+			uiHandler.sendMessage(uimsg);
+	}
+	
+
+
+	/***************************************************** 请求业务 ********************************/
+
+	/**
+	 * 获取品牌列表
+	 * @param params
+	 */
+	public void getBrands(HashMap<String, String> params) {
+		String url = super.getUrl(Method_Base_Brands, "", params);
+		volley.request(url, Msg.M_Base_Brands);
+	}
+
+	/**
+	 * 获取车系列表
+	 * @param params
+	 */
+	public void getSeries(HashMap<String, String> params) {
+		String url = super.getUrl(Method_Base_Series, "", params);
+		volley.request(url, Msg.M_Base_Series);
+	}
+
+	/**
+	 * 获取车款列表
+	 * @param params
+	 */
+	public void getTypes(HashMap<String, String> params) {
+		String url = super.getUrl(Method_Base_types, "", params);
+		volley.request(url, Msg.M_Base_Types);
+	}
+}	
+
+ 
--- src/com/wicare/wistorm/http/Msg.java
+++ src/com/wicare/wistorm/http/Msg.java
@@ -12,6 +12,14 @@ package com.wicare.wistorm.http;
  * @author c TODO 2015-12-11
  */
 public class Msg {
+	
+	/**
+	 * 基础模块
+	 */
+	public final static int M_Base_Brands = 2000;// 获取品牌列表(非活跃数据1d)
+	public final static int M_Base_Series = 2001;// 获取车系列表(非活跃数据1d)
+	public final static int M_Base_Types = 2002;// 获取车款列表
+	
 
 	/**
 	 * 用户模块
@@ -19,11 +27,11 @@ public class Msg {
 	public final static int M_Usr_Register = 100;// 用户注册接口
 	public final static int M_Usr_Psd_Reset = 1000;// 用户密码重置
 	public final static int M_Usr_Volid_Code = 1001;// 验证短信验证码是否正确
-	
+
 	public final static int M_Usr_Login = 101;// 用户登陆接口
 	public final static int M_Usr_Third_Login = 1011;// 第三方登陆接口
 	public final static int M_Usr_Delete = 102;// 用户注销接口
-	
+
 	public final static int M_Usr_Create = 1030;// 创建用户接口
 	public final static int M_Usr_Get = 1031;// 用户信息接口
 	public final static int M_Usr_Update = 1032;// 修改用户信息接口
@@ -40,15 +48,27 @@ public class Msg {
 	public final static int M_Usr_Pois_List = 114;// 获取用户下属所有兴趣点或电子围栏
 	public final static int M_Usr_Pois_Search = 115;// 搜索用户下属所有兴趣点或电子围栏
 	public final static int M_Usr_poi_Get = 116;// 获取兴趣单或者电子围栏信息
-	
-	public final static int M_Biz_Create = 117;// 业务信息
-	public final static int M_Biz_Update = 118;// 业务信息
-	
-	public final static int M_Biz_Update_Status = 119;// 业务信息
-	public final static int M_Biz_List = 120;// 业务信息
-	public final static int M_Biz_Total = 121;// 业务信息
-	
-	
+
+	/**
+	 * 业务模块
+	 */
+	public final static int M_Biz_Create = 120;// 业务信息
+	public final static int M_Biz_Update = 121;// 业务信息
+
+	public final static int M_Biz_Update_Status = 122;// 业务信息
+	public final static int M_Biz_List = 123;// 业务信息
+	public final static int M_Biz_Total = 124;// 业务信息
+
+	/**
+	 * 车辆接口
+	 */
+	public final static int M_Vehicle_Create = 133;// 新增车辆
+	public final static int M_Vehicle_Update = 134;// 修改车辆
+	public final static int M_Vehicle_Get = 135;// 获取车辆信息
+	public final static int M_Vehicle_Delete = 136;// 删除车辆
+	public final static int M_Vehicle_List = 137;// 车辆列表
+	public final static int M_Vehicle_Search = 138;// 搜索车辆
+
 	/**
 	 * 5.通讯接口 comm
 	 */
